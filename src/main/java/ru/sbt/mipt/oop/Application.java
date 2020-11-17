15d71c9ccb269f1506cd3395677e04f4be7a6c34
abdrakhimov1 <dan.abdrakhimov@yandex.ru>
2020-11-17 19:22:59 +0300

    Протащил изменения из предыдущих дз + добавил изменения по комментариям

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,17 +1,15 @@
 package ru.sbt.mipt.oop;
 
 import com.coolcompany.smarthome.events.SensorEventsManager;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import ru.sbt.mipt.oop.spring.utils.SpringConfiguration;
 
-@SpringBootApplication
+
 public class Application {
     public static void main(String... args) {
-        SpringApplication.run(Application.class, args);
-        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
         SensorEventsManager sensorEventsManager = context.getBean(SensorEventsManager.class);
         sensorEventsManager.start();
     }
